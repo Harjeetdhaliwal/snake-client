890f5bbe9e4da77343ca9b4d7b88f69019f01e73
Harjeet Kaur <harjeet.kr1995@gmail.com>
2021-08-25 13:57:55 -0400

    Created setupInput function to handle user inputs

--- play.js
+++ play.js
@@ -1,4 +1,25 @@
+const { stdin } = require("process");
 const {connect} = require("./client");
 
 console.log("Connecting....");
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf-8");
+  stdin.resume();
+  return stdin;
+};
+
+
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
+stdin.on("data", handleUserInput);
+
+setupInput();
