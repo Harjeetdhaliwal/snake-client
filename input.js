ac34fc392111ea41b6b57fc371d0e07e9a1fea98
Harjeet Kaur <harjeet.kr1995@gmail.com>
2021-08-25 14:20:15 -0400

    completed last step of sending data to input module

--- input.js
+++ input.js
@@ -1,6 +1,9 @@
 const { stdin } = require("process");
 
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf-8");
@@ -12,6 +15,14 @@ const setupInput = function() {
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
+  } else if (key === "w") {
+    connection.write("Move: up");
+  } else if (key === "a") {
+    connection.write("Move: left");
+  } else if (key === "s") {
+    connection.write("Move: down");
+  } else if (key === "d") {
+    connection.write("Move: right");
   }
 }
 
